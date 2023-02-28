7a6d65dfd7155216e3dc441aef8dcfc1f518ee36
lukehaag <1918833@students.wits.ac.za>
2023-02-28 22:51:44 +0200

    my message

--- auth.py
+++ auth.py
@@ -71,7 +71,7 @@ def check_permissions(permission, payload):
 
 
 def verify_decode_jwt(token):
-    jsonurl = urlopen(f'https://lkhg.us.auth0.com/.well-known/jwks.json')
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
